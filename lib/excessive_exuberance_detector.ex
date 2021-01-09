16f33f8dc46ddb03e7b93806d100ca15b04b736f
Brett Wise <brettdwise@gmail.com>
2021-01-09 13:45:33 -0500

    Adds updated top level entry point into app

--- lib/excessive_exuberance_detector.ex
+++ lib/excessive_exuberance_detector.ex
@@ -1,18 +1,32 @@
 defmodule ExcessiveExuberanceDetector do
   @moduledoc """
-  Documentation for `ExcessiveExuberanceDetector`.
+  The entry point for running this app.
   """
 
+  alias ExcessiveExuberanceDetector.{
+    ConsolePrinter,
+    ExuberanceRater,
+    SiteScraper
+  }
+
   @doc """
-  Hello world.
+  Get the top 3 overly positive reviews from the first 5 McKaig review pages
 
   ## Examples
 
-      iex> ExcessiveExuberanceDetector.hello()
-      :world
+      iex> ExcessiveExuberanceDetector.rank_reviews()
+      -----------------------------------------------
+      |        Top 3 Overly Positive Reviews        |
+      |                  ----------                 |
+      | #1. It was the best service ever!!!!!!!!    |
+      | #2. Amazing service!                        |
+      | #3. Helpful staff.                          |
+      _______________________________________________
 
   """
-  def hello do
-    :world
+  def rank_reviews do
+    SiteScraper.scrape_reviews()
+    |> ExuberanceRater.rate_reviews()
+    |> ConsolePrinter.print_top_three_to_console()
   end
 end
