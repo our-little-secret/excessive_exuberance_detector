96aeb58bbb4d618215714123e656381eba2987be
Brett Wise <brettdwise@gmail.com>
2021-01-09 22:01:39 -0500

    Adds docs and formatting

--- lib/site_scraper/site_scraper.ex
+++ lib/site_scraper/site_scraper.ex
@@ -1,4 +1,8 @@
 defmodule ExcessiveExuberanceDetector.SiteScraper do
+  @moduledoc """
+  Handles scraping reviews from dealerrater.com for McKaig Chevrolet Buick and
+  prepares them to be passed to the `ExuberanceRater` to be rated.
+  """
   alias ExcessiveExuberanceDetector.Constants
 
   @review_request_service Application.get_env(
@@ -7,20 +11,21 @@ defmodule ExcessiveExuberanceDetector.SiteScraper do
                           )
 
   @doc """
-  Scrapes the first 5 pages of reviews for their contents from dealerrater.com
-  for McKaig Chevrolet Buick
+  Scrapes the first 5 pages of reviews for the review contents from dealerrater.com
+  for McKaig Chevrolet Buick.
   """
   @spec scrape_reviews :: [String.t()]
   def scrape_reviews do
-    get_reviews_pages()
+    Constants.first_five_mckaig_review_page_urls()
+    |> get_review_pages
     |> parse_reviews
   end
 
   @doc """
   Takes a list of URLs and returns a list of the HTML bodies.
   """
-  @spec get_reviews_pages([String.t()]) :: [String.t()]
-  def get_reviews_pages(pages_to_get \\ Constants.first_five_mckaig_review_page_urls()) do
+  @spec get_review_pages([String.t()]) :: [String.t()]
+  def get_review_pages(pages_to_get) do
     pages_to_get
     |> Enum.map(&get_single_reviews_page/1)
   end
@@ -47,7 +52,8 @@ defmodule ExcessiveExuberanceDetector.SiteScraper do
   end
 
   @doc """
-  Strips away additional elements leaving only reviews.
+  Takes a list of Floki HTML tree representations and returns just the content
+  of the reviews.
   """
   @spec return_just_reviews([Floki.html_tree()]) :: [String.t()]
   def return_just_reviews(reviews) do
@@ -55,7 +61,21 @@ defmodule ExcessiveExuberanceDetector.SiteScraper do
     |> Enum.map(&return_just_review/1)
   end
 
-  defp return_just_review({_tag, _class_list, [review]}), do: review
+  @doc """
+  Takes a Floki HTML tree representation and returns just the review content
+  while stripping out display characters.
+  """
+  @spec return_just_review(Floki.html_tree()) :: String.t()
+  def return_just_review({_tag, _class_list, [review]}) do
+    review
+    |> replace_return_newlines_with_spaces
+    |> String.trim
+  end
+
+  defp replace_return_newlines_with_spaces(review) do
+    review
+    |> String.replace("\r\n", " ")
+  end
 
   @doc """
   Takes a URL and makes an HTTP request to get the HTML.
