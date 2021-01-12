24b337a04105edd7d3512dcd5ebabd84bcb43de0
Brett Wise <brettdwise@gmail.com>
2021-01-12 00:19:29 -0500

    Adds docs and moves IO into module

--- lib/site_scraper/review_request_service.ex
+++ lib/site_scraper/review_request_service.ex
@@ -1,9 +1,15 @@
 defmodule ExcessiveExuberanceDetector.SiteScraper.ReviewRequestService do
   @moduledoc """
-  Handles HTTP requests to review sites
+  Handles HTTP requests to review sites.
   """
 
+  @doc """
+  Takes a URL and makes an HTTP request to get the HTML.
+  """
+  @spec get_reviews_page(String.t()) :: String.t() | :ok
   def get_reviews_page(url) do
+    IO.puts("Scraping #{get_page(url)}...")
+
     case HTTPoison.get(url) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
         body
@@ -16,4 +22,6 @@ defmodule ExcessiveExuberanceDetector.SiteScraper.ReviewRequestService do
         )
     end
   end
+
+  defp get_page(url), do: url |> String.split("/") |> Enum.at(5)
 end
