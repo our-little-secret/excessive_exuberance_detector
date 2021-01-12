49fc7a41788cd66e628c50455a138c06b6a7e4da
Brett Wise <brettdwise@gmail.com>
2021-01-12 00:19:48 -0500

    Correct docs

--- lib/excessive_exuberance_detector.ex
+++ lib/excessive_exuberance_detector.ex
@@ -10,19 +10,8 @@ defmodule ExcessiveExuberanceDetector do
   }
 
   @doc """
-  Get the top 3 overly positive reviews from the first 5 McKaig review pages
-
-  ## Examples
-
-      iex> ExcessiveExuberanceDetector.rank_reviews()
-      -----------------------------------------------
-      |        Top 3 Overly Positive Reviews        |
-      |                  ----------                 |
-      | #1. It was the best service ever!!!!!!!!    |
-      | #2. Amazing service!                        |
-      | #3. Helpful staff.                          |
-      _______________________________________________
-
+  Gets the top 3 overly positive reviews from the first 5 McKaig review pages
+  and prints the results to the console
   """
   def rank_reviews do
     SiteScraper.scrape_reviews()
